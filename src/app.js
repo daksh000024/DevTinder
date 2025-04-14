1d7a6cd620b632a98588a2aa37e855fbe583d5b8
Daksh <dakshgaur369@gmail.com>
2025-04-14 18:39:24 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
-app.use("/",(req,res)=>{
-  res.send("You are on right port ");
+app.get("/hello/2",(req,res)=>{
+  res.send("Hello 2je bhi Bhaiiiii");
+});   
+app.get("/hello",(req,res)=>{
+  res.send("Hello Bhaiiiiii");
 });
 app.use("/test",(req,res)=>{
     res.send("You topped the test");
-  });
-app.get("/hello",(req,res)=>{
-    res.send("Hello Bhaiiiiii");
-  });  
+});
+app.use("/",(req,res)=>{
+  res.send("You are on right port ");
+});
+
 app.listen(7777,()=>{
   console.log('Hello......');
 });
